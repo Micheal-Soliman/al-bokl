4403ac6e76c11ea1c2495b06a02e9dd527bffef9
Ahmedkassem7 <ahmedmohamed010134@gmail.com>
2025-10-29 17:18:28 +0300

    Add script px

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Cairo } from "next/font/google";
 import "./globals.css";
 import GoogleAnalytics from "./components/analytics/GoogleAnalytics";
+import MetaPixel from "./components/analytics/MetaPixel";
 
 const cairo = Cairo({
   variable: "--font-cairo",
@@ -20,11 +21,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   // Add your Google Analytics Measurement ID here
   const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+  const META_PIXEL_ID = '2863839303813679';
   
   return (
     <html lang="ar" dir="rtl">
       <body className={cairo.variable}>
         {GA_MEASUREMENT_ID && <GoogleAnalytics measurementId={GA_MEASUREMENT_ID} />}
+        <MetaPixel pixelId={META_PIXEL_ID} />
         {children}
         
       </body>
